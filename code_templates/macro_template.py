18fda6dad8febaa518fe4701ab647014eee5247c
Diogo Jesus <72945621+48302-DiogoJesus@users.noreply.github.com>
2024-06-19 21:25:21 +0100

    Update to conform with package

--- code_templates/macro_template.py
+++ code_templates/macro_template.py
@@ -1,6 +1,6 @@
 from DesktopAutomationFramework import vars, keyboard, key, windows, files, gui, Macro, wait, end
 
-@Macro(interval_s=2.5) # You can increase this while testing and decrease later
+@Macro() # You can increase this while testing and decrease later
 def macro():
     # GETTING INVOCATION VARIABLES #
 
